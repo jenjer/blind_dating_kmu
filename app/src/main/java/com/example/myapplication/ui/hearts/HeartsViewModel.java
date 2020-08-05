887b75e33c85d7e5fe8efe38fc285d60c46c7e74
JeongHooKwon <kjhh03@gmail.com>
2020-08-05 23:09:50 +0900

    JeongHoo - 2020.08.05 : Convert from Cotlin to Java

---
+++ app/src/main/java/com/example/myapplication/ui/hearts/HeartsViewModel.java
@@ -0,0 +1,19 @@
+package com.example.myapplication.ui.hearts;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HeartsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HeartsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is notifications fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
