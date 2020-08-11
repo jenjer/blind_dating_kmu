87dee8f33b45ec9b6a50196b2b73bb3078bec5d1
harve-kim <56036506+harve-kim@users.noreply.github.com>
2020-08-11 15:13:11 +0900

    ㅁㅁ
    틀

--- app/src/main/java/com/example/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -4,17 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class SplashActivity extends AppCompatActivity {
+/*
+    변수명 = 설명
+    IDtext = id 적는 텍스트박스
+    loginbutton = login버튼
+    PWtext = pw 적는 텍스트박스
+ */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        EditText IDtext = (EditText)findViewById(R.id.editTextTextPersonName);
+        EditText PWtext = (EditText)findViewById(R.id.editTextTextPassword);
         Button loginButton = (Button) findViewById(R.id.login);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
