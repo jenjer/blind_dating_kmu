8ac63b176c78ab02098fd1e35667b40330dcbcf5
JeongHooKwon <kjhh03@gmail.com>
2020-08-12 19:27:18 +0900

    JeongHoo - 2020.08.12 : Change Action Bar Button by Fragment

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
