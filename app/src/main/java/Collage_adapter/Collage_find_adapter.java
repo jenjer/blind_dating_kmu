2d83072b5156267bafba311fb63291ace3fcdb77
harve-kim <56036506+harve-kim@users.noreply.github.com>
2020-08-20 13:35:27 +0900

    Merge branch 'master' of E:\Blind_dating_self_project\blind_dating_kmu with conflicts.

---
+++ app/src/main/java/Collage_adapter/Collage_find_adapter.java
@@ -0,0 +1,106 @@
+package Collage_adapter;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.*;
+import com.example.myapplication.ui.splash.RegisterActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+
+public class Collage_find_adapter extends BaseAdapter {
+
+    // Declare Variables
+    Context context;
+    LayoutInflater inflater;
+    private List<String> Colage_List = null;
+    private ArrayList<String> arrayList;
+    public Collage_find_adapter(Context context, List<String> Colage_List) {
+        this.context = context;
+        this.Colage_List = Colage_List;
+        inflater = LayoutInflater.from(context);
+        this.arrayList = new ArrayList<String>();
+        this.arrayList.addAll(Colage_List);
+    }
+
+    public class ViewHolder {
+        TextView tv_name;
+        ImageView iv_icon;
+    }
+
+    @Override
+    public int getCount() {
+        return Colage_List.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return Colage_List.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View view, ViewGroup parent) {
+        final ViewHolder holder;
+        final String Collage = Colage_List.get(position);
+
+
+        if (view == null) {
+            holder = new ViewHolder();
+            view = inflater.inflate(R.layout.row_collage, null);
+            // Locate the TextViews in listview_item.xml
+            holder.tv_name = (TextView) view.findViewById(R.id.textView_row);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+        // Set the results into TextViews
+        holder.tv_name.setText(Collage);
+
+        // Listen for ListView Item Click
+        view.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(context, RegisterActivity.class);;
+                context.startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+
+    // Filter Class
+    public void filter(String charText) {
+        charText = charText.toLowerCase(Locale.getDefault());
+        Colage_List.clear();
+        if (charText.length() == 0) {
+            Colage_List.addAll(arrayList);
+        } else {
+            for (String potion : arrayList) {
+//                String name = context.getResources();
+ //               if (name.toLowerCase().contains(charText)) {
+                    Colage_List.add(potion);
+                }
+            }
+        }
+  //      notifyDataSetChanged();
+}
+
+
--- app/src/main/java/com/example/myapplication/ui/splash/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/ui/splash/RegisterActivity.java
@@ -1,23 +1,59 @@
 package com.example.myapplication.ui.splash;
-
+import com.example.myapplication.*;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
-import com.example.myapplication.SplashActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import Collage_adapter.Collage_find_adapter;
 
 public class RegisterActivity extends AppCompatActivity {
+   private Collage_find_adapter adapter;
+    public void init(){
+        List<String> Collage_list = new ArrayList<String>();
+        Collage_list.add("계명대");
+        Collage_list.add("계명대1");
+        Collage_list.add("계명대2");
+        adapter = new Collage_find_adapter(this,Collage_list);
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        init();
+        EditText EditText_find_collage = (EditText)findViewById(R.id.EditText_find_collage);
+        EditText_find_collage.addTextChangedListener(new TextWatcher(){
+            @Override
+            public void afterTextChanged(Editable arg0){
+                String text = EditText_find_collage.getText().toString().toLowerCase(Locale.getDefault());
+                adapter.filter(text);
+            }
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3){
+
+            }
+            @Override
+            public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3){
+
+            }
+        });
+
+
+
+
         // 레지스터 버튼 및 기능
         Button registerButton = (Button)findViewById(R.id.register_complete_button);
         registerButton.setOnClickListener(new View.OnClickListener() {
